aef1a6a5bef0bb603de081ce0212de0ae0cf5707
Yaiotaeri <testevscode5566@gmail.com>
2026-02-12 19:59:15 -0300

    Implement responsive styles for Menu
    Add responsive styles for Menu component.

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -38,4 +38,9 @@ export const MyButton = styled(Link)`
         transform:scale(0.90);
         background: #012455;
     }
+
+    @media (max-width: 768px) {
+    width:6rem;
+    height:2rem;
+  }
 `
