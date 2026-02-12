653a6c4817ec174832e7ff3e18713441e6d1f111
Yaiotaeri <testevscode5566@gmail.com>
2026-02-12 19:47:46 -0300

    Fix import case for IconCards component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import IconCards from "../../components/IconCards";
+import IconCards from "../../components/iconCards";
 import Menu from "../../components/Menu";
 import Footer from "../../components/Footer/Footer";
 export default function Home() {
